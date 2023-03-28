26c1b4142b6bdb7b8a29528dafe21ed927600a4f
Vranox <damyscus@gmail.com>
2023-03-28 03:20:58 +0200

    ne fonctionne pas

--- app/src/main/java/com/example/myapplication/models/establishment.java
+++ app/src/main/java/com/example/myapplication/models/establishment.java
@@ -2,6 +2,8 @@ package com.example.myapplication.models;
 
 public class establishment {
     public String title;
+    public String imageName;
     public String description;
-    public 
+    public String horaires;
+
 }
