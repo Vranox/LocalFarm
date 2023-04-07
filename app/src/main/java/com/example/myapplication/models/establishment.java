d965f933526241855273aa87b219b2e05249c308
Vranox <damyscus@gmail.com>
2023-04-07 17:25:31 +0200

    zr

--- app/src/main/java/com/example/myapplication/models/establishment.java
+++ app/src/main/java/com/example/myapplication/models/establishment.java
@@ -1,6 +1,6 @@
 package com.example.myapplication.models;
 
-public class establishment {
+public class Establishment {
     public String title;
     public String imageName;
     public String description;
