2aedfc25a6dc54e1d109a2bb1f0377e2c94b42a8
simonbeurel <simon.beurel@etu.univ-cotedazur.fr>
2023-05-03 10:45:22 +0100

    First part of the tweets show

--- app/src/main/java/com/example/localfarm/activity/TweetsActivity.java
+++ app/src/main/java/com/example/localfarm/activity/TweetsActivity.java
@@ -3,6 +3,7 @@ package com.example.localfarm.activity;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,7 +19,10 @@ import com.example.localfarm.R;
 import com.example.localfarm.models.Establishment;
 import com.example.localfarm.models.Tweet;
 import com.example.localfarm.recyclerview.EstablishmentAdapter;
+import com.example.localfarm.recyclerview.TweetsAdapter;
 import com.google.android.gms.common.api.Status;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -33,12 +37,63 @@ import java.util.List;
 
 public class TweetsActivity extends Activity {
 
-    private ListView listViewTweets;
+    private RecyclerView mRecyclerView;
+    private TweetsAdapter mAdapter;
+    private List<Tweet> mTweets;
+    private DatabaseReference mDatabase;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tweets);
+
+        // Configuration de la référence à la base de données Firebase
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        // Création des faux tweets
+        mTweets = new ArrayList<>();
+        mTweets.add(new Tweet("Alice", "Hello, Twitter!", "2023-05-03"));
+        mTweets.add(new Tweet("Bob", "This is a fake tweet.", "2023-05-02"));
+        mTweets.add(new Tweet("Charlie", "I'm tweeting from Java Android!", "2023-05-01"));
+
+        // Configuration de la RecyclerView avec l'adapter
+        mRecyclerView = findViewById(R.id.recycler_view_tweets);
+        mAdapter = new TweetsAdapter(mTweets, this);
+        mRecyclerView.setAdapter(mAdapter);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        // Ajout d'un listener sur le bouton flottant pour ajouter un nouveau tweet
+        FloatingActionButton fab = findViewById(R.id.post_tweet);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Création d'un nouveau tweet et ajout à Firebase
+                Tweet newTweet = new Tweet("David", "This is a new tweet!", "2023-05-04");
+                mDatabase.child("tweets").push().setValue(newTweet)
+                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                // Récupération du tweet depuis Firebase
+                                mDatabase.child("tweets").addListenerForSingleValueEvent(new ValueEventListener() {
+                                    @Override
+                                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                        // Ajout du dernier tweet à la liste de tweets
+                                        Tweet tweet = dataSnapshot.getChildren().iterator().next().getValue(Tweet.class);
+                                        mTweets.add(tweet);
+                                        // Notification à l'adapter que la liste de tweets a été mise à jour
+                                        mAdapter.notifyDataSetChanged();
+                                    }
+
+                                    @Override
+                                    public void onCancelled(@NonNull DatabaseError databaseError) {
+                                        Log.w("Simon_Tweet", "loadPost:onCancelled", databaseError.toException());
+                                    }
+                                });
+                            }
+                        });
+            }
+        });
     }
 
 
