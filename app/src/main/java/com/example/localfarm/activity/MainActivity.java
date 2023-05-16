dc09e43cbc3b6e12a981a965e29dfe49be1f62ca
marco <marco.laclavere@etu.unice.fr>
2023-05-16 16:44:15 +0200

    testing thingies

--- app/src/main/java/com/example/localfarm/activity/MainActivity.java
+++ app/src/main/java/com/example/localfarm/activity/MainActivity.java
@@ -1,36 +1,16 @@
 package com.example.localfarm.activity;
 
-import android.content.Context;
-import android.content.Intent;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import com.example.localfarm.R;
 import com.example.localfarm.databinding.ActivityMainBinding;
-import com.example.localfarm.models.Establishment;
-import com.example.localfarm.models.Schedule;
-import com.example.localfarm.models.Time;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.FirebaseApp;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-
-import java.time.LocalTime;
 
 public class MainActivity extends AppCompatActivity {
 
