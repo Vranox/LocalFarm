6acd1229f7955b69d500bbbe15c613cd09436419
Vranox <damyscus@gmail.com>
2023-05-16 15:33:05 +0200

    Now an establishment can be added and we can see the schedule and distance on the main menu

--- app/src/main/java/com/example/localfarm/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/localfarm/ui/home/HomeFragment.java
@@ -9,29 +9,9 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
-
-import com.example.localfarm.R;
 import com.example.localfarm.databinding.FragmentHomeBinding;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
-
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.tasks.Task;
-
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
 
 public class HomeFragment extends Fragment{
 
